7e5d567734711574635d78b7eb682db4481c4f87
talama <talama@proton.me>
2025-10-10 10:57:17 +0200

    feat: updated example for sockets

--- pyproto/protocols/test.py
+++ pyproto/protocols/test.py
@@ -1,12 +1,11 @@
 import socket
 
 from .icmp import ICMPEcho, ICMPType
+from .sockets import ICMPSocket
 
 pkt = ICMPEcho(ICMPType.ECHO_REQUEST)
-with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as s:
-    s.sendto(pkt.to_bytes(), ("8.8.8.8", 0))
-    res = s.recv(1024)
-    ip_length = (res[0] & 0x0F) * 4
-    icmp_raw = res[ip_length:]
-    icmp_pkt = ICMPEcho.from_bytes(icmp_raw)
-    print(icmp_pkt)
+
+with ICMPSocket(dest="8.8.8.8") as s:
+    s.send(pkt)
+    reply, addr, rtt = s.receive()
+    print(f"Received {reply} from {addr} in time={rtt:.2f}ms")
