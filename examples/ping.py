dbc8533f0889c0b9a20a72e69e1eaf59ab0cd412
talama <talama@proton.me>
2025-10-21 22:28:36 +0200

    chore: updated ping example

--- examples/ping.py
+++ examples/ping.py
@@ -4,5 +4,12 @@ from pyproto import ping, set_log_level
 
 if __name__ == "__main__":
     set_log_level(logging.ERROR)
-    result = ping(dest="8.8.8.8")
-    print(result)
+    result = ping(dest="8.8.8.8", output=True)
+    packet_loss = float(result.lost / result.sent) * 100
+    print("\n\n")
+    print(f"--- {result.dest} ping statistics ---")
+    print(
+        f"{result.sent} packet transmitted, {result.recvd} received, {packet_loss:.0f}% packet loss."
+    )
+    for p in result.packets:
+        print(p)
