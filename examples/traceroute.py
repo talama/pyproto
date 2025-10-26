7dd5d5de0819139bdec9727304034094e71e392b
talama <talama@proton.me>
2025-10-26 22:14:22 +0100

    chore: updated example

--- examples/traceroute.py
+++ examples/traceroute.py
@@ -1,4 +1,8 @@
 from pyproto import traceroute
 
 if __name__ == "__main__":
-    hops = traceroute(dest="8.8.8.8", output=True)
+    result = traceroute(dest="8.8.8.8", output=True)
+
+    # print(f"traceroute to {result.dest}")
+    # for hop in result.hops:
+    #     print(hop.to_line())
